a5d6021bdcadfe5ec114a1edc324f9108aa9a98f
ong-ar <reclusis@gmail.com>
2018-03-22 00:30:28 +0900

    add transaction.js

---
+++ src/transaction.js
@@ -0,0 +1,30 @@
+class TxOut {
+    constructor(address, amount) {
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+class TxIn {
+    //uTxOutId
+    //uTxOutIndex
+    //signature
+
+}
+
+class Transaction {
+    //ID
+    //txIns[]
+    //txOuts[]
+}
+
+class UTxOut {
+    constructor(uTxOutId, uTxOutIndex, address, amount) {
+        this.uTxOutId = uTxOutId;
+        this.uTxOutIndex = uTxOutIndex;
+        this.address = address;
+        this.amount = amount;
+    }
+}
+
+let uTxOuts = [];
